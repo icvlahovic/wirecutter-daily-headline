53a292e73943f042705c8ebd4edae192bbc875af
icvlahovic <banddorkfht7@gmail.com>
2018-05-04 16:35:41 -0400

    basic scraping implemented into Article

--- lib/wirecutter_daily_headline.rb
+++ lib/wirecutter_daily_headline.rb
@@ -1,3 +1,7 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 require_relative "wirecutter_daily_headline/version"
 require_relative "./wirecutter_daily_headline/cli"
 require_relative "./wirecutter_daily_headline/article"
--- lib/wirecutter_daily_headline/article.rb
+++ lib/wirecutter_daily_headline/article.rb
@@ -3,11 +3,15 @@ class WirecutterDailyHeadline::Article
   attr_accessor :title, :author, :url
 
   def self.today
+    @doc = Nokogiri::HTML(open("https://thewirecutter.com"))
+
     article = self.new
-    article.title = "The Best VR Headsets for PC"
-    article.author = "Signe Brewster"
-    article.url = "https://thewirecutter.com/reviews/best-desktop-virtual-reality-headset/"
+    article.title = @doc.search("h2.headline").first.text.strip
+    article.author = @doc.search("p.author").first.text.strip
+    article.url = @doc.search("a.headline-link").map {|link| link.text.strip}
     puts article.title
+    puts article.author
+    puts article.url
   end
 
 end
